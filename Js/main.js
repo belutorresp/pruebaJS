6270e1897ffe3ed30807552409d77d8d4153b69b
belutorresp <belutorresp@gmail.com>
2021-09-02 11:16:32 -0300

    intento 1 de entregable 1

---
+++ Js/main.js
@@ -0,0 +1,14 @@
+// ESTO ES UN COMENTARIO
+
+let nombre = prompt ('Ingrese su nombre');
+alert(nombre);
+
+let edad = prompt('Ingrese su edad');
+let pasarvalor = parseInt(edad);
+let numero = 50;
+
+let resultado = edad + numero;
+
+alert('Hola' + ' ' + nombre + ' ' + 'tu edad en 50 años será');
+alert(pasarvalor + numero)
+
